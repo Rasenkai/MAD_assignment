6cfc238f941a901f5067c3d433694c4cdaa22574
Rasenkai <rasenkai99@gmail.com>
2022-10-14 17:14:33 +0530

    library: redo login and signup
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- app/src/main/java/com/rasenkai/library/AddActivity.java
+++ app/src/main/java/com/rasenkai/library/AddActivity.java
@@ -1,5 +1,6 @@
 package com.rasenkai.library;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,5 +31,7 @@ public class AddActivity extends AppCompatActivity {
                         Integer.valueOf(price_input.getText().toString().trim()));
             }
         });
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
